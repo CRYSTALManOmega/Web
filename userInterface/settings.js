66150d08e8a7b3b20e7244fd212485a6695d5979
hesham <hesham.h.h@windowslive.com>
2024-11-16 19:11:23 +0300

    Delete userInterface directory

--- userInterface/settings.js
+++
@@ -1,5 +0,0 @@
-// Function to toggle settings dropdown menu
-function toggleSettingsMenu() {
-    const dropdown = document.getElementById('settings-dropdown');
-    dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
-}
